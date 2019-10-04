a0e110d16e2a1f9591cbedf5aa74261f0ca19d15
Sandra Derbring <sandra.derbring@gmail.com>
2019-10-04 21:42:46 +0200

    final

--- lab4/weather/device.py
+++ lab4/weather/device.py
@@ -5,25 +5,8 @@ import json
 
 class WeatherDevice(DddDevice):
 
-
-    UNITS = {
-        "celsius":"metric",
-        "fahrenheit":"imperial",
-        "metric":"metric",
-        "imperial":"imperial"
-    }
-
-    def getData(self, city, country):
-        url = 'http://api.openweathermap.org/data/2.5/weather?q=%s,%s&APPID=d258b72f49da373d5ee9a33f525d1252' % (city,country)
-        #print url                                                                                                                                                                  
-        request = Request(url)
-        response = urlopen(request)
-        data = response.read()
-        return json.loads(data)
-
-    def getUnitData(self, city, country, unit):
+    def getData(self, city, country, unit="metric"):
         url = 'http://api.openweathermap.org/data/2.5/weather?q=%s,%s&units=%s&APPID=d258b72f49da373d5ee9a33f525d1252' % (city,country,unit)
-        #print url                                                                                                                                                                
         request = Request(url)
         response = urlopen(request)
         data = response.read()
@@ -31,9 +14,8 @@ class WeatherDevice(DddDevice):
 
     class temperature(DeviceWHQuery):
         def perform(self, city, country, unit):
-            data = self.device.getUnitData(city, country, self.device.UNITS.get(unit))
+            data = self.device.getData(city, country, unit)
             temp = data['main']['temp']
-            print('it\'s %d degrees in %s') % (temp, city)
             tempstr = str(temp)
             return [tempstr]
 
@@ -41,6 +23,5 @@ class WeatherDevice(DddDevice):
         def perform(self, city,country):
             data = self.device.getData(city, country)
             weather = data['weather'][0]['description']
-            print('%s in %s') % (weather, city)
             weatherstr = str(weather)
             return [weatherstr]
