da41834d37695acb01a9fdc7be15f25dec1802ea
Sandra Derbring <sandra.derbring@gmail.com>
2019-11-04 20:47:39 +0100

    testing parameters with options

---
+++ project/experiments/lab2/call_john_ddd_2019/call_john/device.py
@@ -0,0 +1,64 @@
+from tdm.lib.device import DddDevice, DeviceAction, DeviceWHQuery, Validity, EntityRecognizer
+
+
+class CallJohnDevice(DddDevice):
+
+    JOHN = "contact_john"
+    LISA = "contact_lisa"
+    MARY = "contact_mary"
+    ANDY = "contact_andy"
+
+    MOBILE = "mobile"
+    WORK = "work"
+    HOME = "home"
+
+    PHONE_NUMBERS = {
+        JOHN: {
+            MOBILE: "0701234567",
+            WORK: "0736582934",
+            HOME: "031122363"
+        },
+        LISA: {
+            MOBILE: "0709876543",
+            WORK: "0763559230",
+            HOME: "031749205"
+        },
+        MARY: {
+            MOBILE: "0706574839",
+            WORK: "0784736475",
+            HOME: "031847528"
+            },
+        ANDY: None
+    }
+
+    CONTACTS = {
+        "John": JOHN,
+        "Lisa": LISA,
+        "Mary": MARY,
+        "Andy": ANDY,
+    }
+
+    class Call(DeviceAction):
+        def perform(self, selected_contact, phone_type):
+            return True
+
+    class phone_number(DeviceWHQuery):
+        def perform(self, selected_contact, phone_type):
+            number = self.device.PHONE_NUMBERS.get(selected_contact).get(phone_type)
+            return [number]
+
+    class selected_contact(DeviceWHQuery):
+        def perform(self):
+            names_of_contacts_from_api = ['John']
+            #names_of_contacts_from_api = self.device.CONTACTS.keys()  # = ['John', 'Lisa', 'Mary', 'Andy']
+            names_of_individuals_defined_in_ontology = [self.device.CONTACTS[i] for i in names_of_contacts_from_api]
+            #names_of_individuals_defined_in_ontology = ["contact_john", "contact_lisa", "contact_mary", "contact_andy"]
+            return names_of_individuals_defined_in_ontology
+
+    class PhoneNumberAvailable(Validity):
+        def is_valid(self, selected_contact):
+            contact_numbers = self.device.PHONE_NUMBERS.get(selected_contact)
+            if contact_numbers == None:
+                return False
+            else:
+                return True
